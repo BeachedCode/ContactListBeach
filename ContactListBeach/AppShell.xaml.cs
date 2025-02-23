410d722a805d2db2cfe0b84cd3120e7fe0b1683f
Ethan <Beached@mail.uc.edu>
2025-02-23 09:35:49 -0500

    Add project files.

---
+++ ContactListBeach/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactListBeach
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ContactListBeach/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ContactListBeach
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
